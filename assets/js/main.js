068c7d45bbc33c3c57373dea00ec861ef99fa01b
sebas <rodriguezjs88@gmail.com>
2022-06-13 21:02:49 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function guardarTarea(){
+    var tareaInput = document.getElementById("input-tarea")
+    var listado = document.getElementById("Listado-tareas")
+    listado.innerHTML +=  `<li>${tareaInput.value}</li>`
+    tareainput.value = ""
+}
